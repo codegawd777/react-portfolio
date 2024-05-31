d6d26be18ceb3ae156e5d33c9287c86322402aa5
JustinHinton <justgawd7@icloud.com>
2024-05-31 17:53:13 -0600

    updated layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom';
+import Header from './components/header';
 import './App.css'
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-    
+<Header/>
    <Outlet />
     </>
   )
