d0063a73d0433b1794d5bcd137034155aee6b509
JustinHinton <justgawd7@icloud.com>
2024-05-31 17:37:19 -0600

    added routing system

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Error from './pages/error.jsx';
+import About from './pages/about.jsx';
+import Contact from './pages/contact.jsx';
+import Portfolio from './pages/portfolio.jsx';
+import Resume from './pages/resume.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      { index: true, element: <About /> },
+      { path: 'contact', element: <Contact /> },
+      { path: 'portfolio', element: <Portfolio /> },
+      { path: 'resume', element: <Resume /> },
+    ]
+  },
+
+
+
+])
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
